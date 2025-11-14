b1e3d2af230d3cd39e4a161020e8de47a519e6d5
Jacob Bachmann <jacob.bachmann@posteo.de>
2025-11-14 16:57:59 +0100

    feat: add fzf-inserter

--- lua/config/lazy/fzf-lua.lua
+++ lua/config/lazy/fzf-lua.lua
@@ -15,10 +15,29 @@ return {
                 },
             },
         }
-        require("fzf-lua").setup(opts)
-        vim.keymap.set('n', '<leader>r', ':FzfLua resume<cr>')
-        vim.keymap.set('n', '<leader>g', ':FzfLua grep_project resume=true<cr>')
-        vim.keymap.set('n', '<leader><leader>', ':FzfLua files resume=true<cr>')
-        vim.keymap.set('n', '<leader>f', ':FzfLua git_files resume=true<cr>')
+        local fzf = require("fzf-lua")
+        fzf.setup(opts)
+        vim.keymap.set("n", "<leader>r", ":FzfLua resume<cr>")
+        vim.keymap.set("n", "<leader>g", ":FzfLua grep_project resume=true<cr>")
+        vim.keymap.set("n", "<leader><leader>", ":FzfLua files resume=true<cr>")
+        vim.keymap.set("n", "<leader>f", ":FzfLua git_files resume=true<cr>")
+
+        local items = {}
+
+        local function fzf_insert()
+            fzf.fzf_exec(items, {
+                prompt = "Insert> ",
+                actions = {
+                    ["default"] = function(selected)
+                        if not selected or #selected == 0 then
+                            return
+                        end
+                        vim.api.nvim_put({ selected[1] }, "c", true, true)
+                    end,
+                },
+            })
+        end
+
+        vim.keymap.set("n", "<leader>pp", fzf_insert)
     end,
 }
